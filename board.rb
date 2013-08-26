2a6e2257da6fe5d3140694a6a8fae422b5603822
App Academy Student <student@appacademy.io>
2013-08-26 11:05:48 -0400

    Added gitignore. Board initialization now places mines.

--- board.rb
+++ board.rb
@@ -2,7 +2,32 @@ class Board
   attr_accessor :board
 
   def initialize(dimension)
-    @board = []
-    dimension.times { @board << Array.new(dimension) }
+    self.board = []
+    dimension.times { board << Array.new(dimension, '*'.dup) }
+    mines = mine_initialize(dimension)
   end
+
+  def mine_initialize(dimension)
+
+    mine_positions = []
+
+    if dimension == 9
+      until mine_positions.length == 10
+        row = (0...9).to_a.sample
+        col = (0...9).to_a.sample
+        mine_positions << [row, col] unless board[row][col] == 'm'
+        board[row][col] = 'm'
+      end
+    else
+      until mine_positions.length == 40
+        row = (0...16).to_a.sample
+        col = (0...16).to_a.sample
+        mine_positions << [row, col] unless board[row][col] == 'm'
+        board[row][col] = 'm'
+      end
+    end
+
+    mine_positions
+  end
+
 end
